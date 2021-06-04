6251523a55c396128546d0a8e0de11144d659588
JappaBR <guilherme.dion@gmail.com>
2021-06-04 10:40:28 -0300

    Adicionar arquivos de projeto.

---
+++ memory/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::memory.Properties.Resources.fundo_bob;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(805, 455);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::memory.Properties.Resources.start5;
+            this.button1.Location = new System.Drawing.Point(309, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 47);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(349, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(8, 4);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Memory Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ memory/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Parent = pictureBox1;
+        }
+        PictureBox[] card = new PictureBox[12];
+        int[] vetorNum = new int[12];
+        Random rnd = new Random();
+
+        void GerarVetor()
+        {
+            int numCandidato = 0;
+            int cont = 0;
+            for (int i = 0; i < vetorNum.Length; i++)            
+                vetorNum[i] = 0;
+            for(int i=0; i<12; i++)
+            {
+                do
+                {
+                    numCandidato = rnd.Next(1, 7);
+                    cont = 0;
+                    foreach (int numero in vetorNum)
+                    {
+                        if (numCandidato == numero)
+                            cont++;
+                    }
+                } while (cont == 2);
+                vetorNum[i] = numCandidato;
+                listBox1.Items.Add(vetorNum[i]);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GerarVetor();
+            int x = 0, y = 0;
+            for(int i=0; i<12; i++)
+            {
+                if (x == 400)
+                {
+                    x = 0;
+                    y +=100;
+                }
+
+                card[i] = new PictureBox();
+                card[i].Parent = pictureBox1;
+                card[i].Left += 200 + x;
+                card[i].Top += 50 + y;
+                card[i].Width = 80;
+                card[i].Height = 80;
+                card[i].Load("imagem" + vetorNum[i] + ".png");
+                card[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                x += 100;
+            }
+        }
+    }
+}
